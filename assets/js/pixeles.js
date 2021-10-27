b5c03d118c76c7a814d8dd459afbb870f2ba2686
Diego Araya <diego@dipaola.cl>
2021-10-27 14:17:00 -0300

    actualización pixeles.js

--- assets/js/pixeles.js
+++ assets/js/pixeles.js
@@ -11,9 +11,4 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 fbq('init', '438183797482530');
 fbq('track', 'PageView');
 
-//GTM
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','DC-1019066');
+
